2e6b646756bac79b3eaa1faf629a8d20a4b91c64
najmuleimon <najmul.eimon@gmail.com>
2022-04-29 14:14:59 +0600

    component routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Header from './pages/Shared/Header/Header';
+import Footer from './pages/Shared/Footer/Footer';
+import Home from './pages/Home/Home/Home';
+import Login from './pages/Login/Login/Login';
+import Register from './pages/Login/Register/Register';
+import NotFound from './pages/NotFound/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      this is app js file
-    </div>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/pages/Login/RequireAuth/RequireAuth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RequireAuth = () => {
+    return (
+        <div>
+            this is require auth
+        </div>
+    );
+};
+
+export default RequireAuth;
