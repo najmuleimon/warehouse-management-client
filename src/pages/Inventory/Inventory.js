538ec504e99a2a1c6085aa4bd4c5df6b451810c5
najmuleimon <najmul.eimon@gmail.com>
2022-05-01 23:47:42 +0600

    manage inventories added

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import { Form } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useProduct from '../../hooks/useProduct';
 import './Inventory.css';
 
 const Inventory = () => {
     const {id} = useParams();
+    const navigate = useNavigate();
     const [product, setProduct] = useProduct(id);
 
     // handle deliver item
@@ -56,6 +57,11 @@ const Inventory = () => {
         <div className='single-inventory'>
             <div className="container">
                 <div className="row align-items-center">
+                    <div className="col-lg-12">
+                        <div className="d-flex justify-content-end">
+                            <button onClick={() => navigate('/manage')} className="btn-style">Manage Inventories</button>
+                        </div>
+                    </div>
                     <div className="col-lg-6">
                         <div className="image">
                             <img src={product.image} alt="" />
@@ -71,7 +77,7 @@ const Inventory = () => {
                             <h3>Supplier: {product.supplier}</h3>
                             <h3>Quantity: {product.quantity}</h3>
                             <h3>{product.quantity > 0 ? "on sale" : "sold"}</h3>
-                            <button onClick={handleDelivery}>Delivered</button>
+                            <button onClick={handleDelivery} className="btn-style">Delivered</button>
                         </div>
                     </div>
                     <div className="col-lg-12">
--- src/pages/ManageInventory/ManageInventory.js
+++ src/pages/ManageInventory/ManageInventory.js
@@ -1,10 +1,63 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAllProducts from '../../hooks/useAllProducts';
 import './ManageInventory.css';
 
 const ManageInventory = () => {
+    const [allProducts, setAllProducts] = useAllProducts();
+    const navigate = useNavigate();
+    let count = 1;
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure?');
+        if(proceed){
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                const remaining = allProducts.filter(product => product._id !== id);
+                setAllProducts(remaining);
+            })
+        }
+    }
     return (
-        <div>
-            this is manage inventory
+        <div className='manage-product'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h2>Manage Products</h2>
+                        
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                <th scope="col">Serial</th>
+                                <th scope="col">Product Name</th>
+                                <th scope="col">Product Id</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Update</th>
+                                <th scope="col">Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                allProducts.map(product => <tr key={product._id}>
+                                    <td>{count++}</td>
+                                    <td><h6>{product.name}</h6></td>
+                                    <td><p>{product._id}</p></td>
+                                    <td><h6>{product.category}</h6></td>
+                                    <td><h6>{product.price}</h6></td>
+                                    <td><button onClick={() => navigate(`/inventory/${product._id}`)} className='btn btn-info'>Update</button></td>
+                                    <td><button onClick={() => handleDelete(product._id)} className='btn btn-danger'>Delete</button></td>
+                                </tr>)
+                            }
+                            </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
