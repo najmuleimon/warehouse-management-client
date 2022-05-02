91f4e35587783a8f04138e4e7d3c7d484ca58c4f
najmuleimon <najmul.eimon@gmail.com>
2022-05-02 13:02:27 +0600

    404 page added

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import notFound from '../../images/notfound.jpg';
+import './NotFound.css';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            this is not found
+        <div className='not-found'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="content text-center">
+                            <img src={notFound} alt="" />
+                            <h2>Sorry! We can not find your request</h2>
+                            <button onClick={() => navigate('/')} className="btn-style">Go back Home</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
